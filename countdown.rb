11d4ba7c80ed07b522c9918d0b6685a2cf7a7c89
Anthony Lepore <violacellonyc@gmail.com>
2020-07-28 14:25:47 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,9 +3,11 @@
 def countdown(number)
    while number > 0 
    puts "#{number} SECOND(S)!"
+    number -= 1 
       def countdown_with_sleep
         
-   number -= 1 
+        
+  
  end
  
  return "HAPPY NEW YEAR!"
