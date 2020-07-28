1f26dd815256cbed1c86773645c043f5364ceaaf
Anthony Lepore <violacellonyc@gmail.com>
2020-07-28 14:26:57 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,8 +4,8 @@ def countdown(number)
    while number > 0 
    puts "#{number} SECOND(S)!"
     number -= 1 
-      def countdown_with_sleep
-        sleep(1)
+      def countdown_with_sleep(time)
+        sleep(time.seconds)
       end
       
   
