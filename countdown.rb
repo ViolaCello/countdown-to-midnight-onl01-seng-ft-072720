0a892818a5a18b033d485b48e81d6cb9f213477b
Anthony Lepore <violacellonyc@gmail.com>
2020-07-28 14:28:46 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(number)
+   while number > 0 
+   puts "#{number} SECOND(S)!"
+    number -= 1 
+      def countdown_with_sleep(tempo)
+        sleep(tempo)
+      end
+      
+  
+ end
+ 
+ return "HAPPY NEW YEAR!"
+
+
+
 end
